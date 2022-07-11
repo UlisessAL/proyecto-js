d38cf311ec4981bafa6e58b44de4a17406e01631
¨Ulises¨ <larrabeulises@gmail.com>
2022-07-10 21:35:56 -0300

    Second commit

--- js/main.js
+++ js/main.js
@@ -21,151 +21,24 @@ const menu = [
 ];
 
 
-let bienvenido = alert("El menú se va a ver a continuacion en la consola");
+const tarjetaMenu = document.createElement("section");
 
-console.log(menu);
+tarjetaMenu.className = "carta";
 
-let inicio = prompt("Bienvenido al restaurante FFE, si desea buscar un plato simplemente hagalo aca abajo:");
 
-while (inicio == "") {
-    alert("Por favor ingresa caracteres válidos");
-    inicio = prompt("Bienvenido al restaurante FFE, si desea buscar un plato simplemente hagalo aca abajo:");
+tarjetaMenu.innerHTML = "<h2 class='titulo-lista'>El Menú</h2>\n<ul Id='lista-carta'> </ul>";
+
+document.body.append(tarjetaMenu);
+
+const lista = document.getElementById("lista-carta")
+
+
+let plantilla = `Plato N°: ${menu.id} Nombre: ${menu.nombre} $${menu.precio}`
+
+for (const plantilla of menu){
+    let li = document.createElement("li")
+    li.innerHTML= plantilla
+    li.className = "elementos-lista"
+    lista.appendChild(li)
 }
-
-function filtrarPlato(arr, filtro) {
-    const filtrado = arr.filter ((el) => {
-        return el.nombre.includes(filtro);
-    });
-    return filtrado;
-}
-
-const platoFiltrados = filtrarPlato (menu, inicio);
-
-console.log(platoFiltrados);
-
-
-let porPrecio = prompt("Si desea filtrar por precio máximo, escriba si , del caso contrario escriba otra palabra");
-
-if (porPrecio === "si") {
-
-    let plata = prompt ("Ingresa el monto de precio maximo a filtrar");
-
-function filtrarPorPrecio (arr, filtro) {
-    let filtrado = arr.filter ((el) => {
-        switch (filtro) {
-            case "si":
-
-                
-
-                return el.precio < plata;
-                
-                break;
-        
-        }
-    })
-
-    return filtrado;
-}
-
-const filtroPrecio = filtrarPorPrecio(menu, porPrecio);
-console.log(filtroPrecio);
-
-
-} else {
-    alert("que tenga buen dia");
-}
-
-
-
-function buscarPorNombre (arr, filtro) {
-    const busqueda = arr.filter((el) => {
-        return el.nombre.includes(filtro);
-    });
-    return busqueda;
-}
-
-const carrito = [];
-
-
-
-function cargarAlCarrito (arr, plato) {
-    return  arr.push(plato);
-}
-
-
-
-let buscar = prompt("Si desea buscar un plato para agregar al carrito, directamente escriba el nombre, de caso contrario escriba no");
-
-
-
-if (buscar != "no" || buscar != "") {
-
-
-    const filtrar = filtrarPlato(menu, buscar);
-
-    const encontrado = buscarPorNombre(filtrar, buscar)
-
-    console.log(encontrado);
-    
-    let pregunta = prompt ("Desea agregarlo al carrito? Responda con un si o no")
-    
-    if (pregunta == "si") {
-        cargarAlCarrito(carrito, encontrado)
-    
-    
-    console.log(carrito);
-
-
-    } else {
-        if (buscar == "no") {
-            console.log("Que tenga buen dia");
-        } else {
-            while (buscar == "") {
-                alert("Por favor, ingrese un dato válido");
-                buscar = prompt("Si desea buscar un plato para agregar al carrito, directamente escriba el nombre, de caso contrario escriba no");
-                pregunta = prompt ("Desea agregarlo al carrito? Responda con un si o no");
-            }
-        }
-        
-    }
-
-    let preguntarDenuevo = prompt("Desea agregar otro plato al carrito? Escriba no si no quiere, del caso contrario escriba si");
-
-    
-
-    while (preguntarDenuevo != "no" || buscar == "no") {
-
-        
-
-        buscar = prompt("Escriba el nombre del plato");
-
-    if (buscar != "no") {
-
-        const filtrar = filtrarPlato(menu, buscar);
-        const encontrado = buscarPorNombre(filtrar, buscar);
-
-        console.log(encontrado);
-    
-        pregunta = prompt ("Desea agregarlo al carrito? Responda con un si o no");
-    
-        if (pregunta == "si") {
-        cargarAlCarrito(carrito, encontrado);
-    
-        console.log(carrito);
-
-        } else {
-            console.log("Que tenga buen dia");
-        }
-
-    preguntarDenuevo = prompt("Desea agregar otro plato al carrito? Escriba no si no quiere, del caso contrario escriba si")
-
-    if (preguntarDenuevo == "no") {
-        console.log("Que tenga buen dia");
-    }
-    
-        } else {
-            console.log("Que tenga buen dia");
-        }
-}}
-
 
